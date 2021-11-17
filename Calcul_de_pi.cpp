c0396e309c8baf9ae19f1c0f59f948f7e8636335
Mustapha Bedoui <93264832+Hustler-labX@users.noreply.github.com>
2021-11-17 22:19:55 +0100

    Create Calcul_de_pi.cpp

---
+++ Calcul_de_pi.cpp
@@ -0,0 +1,96 @@
+// les resultats se trouve dans le ficher OutPut000000.txt !! 
+# include <chrono>
+# include <random>
+# include <cstdlib>
+# include <sstream>
+# include <string>
+# include <fstream>
+# include <iostream>
+# include <iomanip>
+# include <mpi.h>
+
+// Attention , ne marche qu'en C++ 11 ou supérieur :
+double approximate_pi( unsigned long nbSamples ) 
+{
+    typedef std::chrono::high_resolution_clock myclock;
+    myclock::time_point beginning = myclock::now();
+    myclock::duration d = beginning.time_since_epoch();
+    unsigned seed = d.count();
+    std::default_random_engine generator(seed);
+    std::uniform_real_distribution <double> distribution ( -1.0 ,1.0);
+    unsigned long nbDarts = 0;
+    // Throw nbSamples darts in the unit square [-1 :1] x [-1 :1]
+    for ( unsigned sample = 0 ; sample < nbSamples ; ++ sample ) {
+        double x = distribution(generator);
+        double y = distribution(generator);
+        // Test if the dart is in the unit disk
+        if ( x*x+y*y<=1 ) nbDarts ++;
+    }
+    // Number of nbDarts throwed in the unit disk
+    double ratio = double(nbDarts)/double(nbSamples);
+    return 4*ratio;
+}
+
+int main( int nargs, char* argv[] )
+{
+	// On initialise le contexte MPI qui va s'occuper :
+	//    1. Créer un communicateur global, COMM_WORLD qui permet de gérer
+	//       et assurer la cohésion de l'ensemble des processus créés par MPI;
+	//    2. d'attribuer à chaque processus un identifiant ( entier ) unique pour
+	//       le communicateur COMM_WORLD
+	//    3. etc...
+	MPI_Init( &nargs, &argv );
+	// Pour des raisons de portabilité qui débordent largement du cadre
+	// de ce cours, on préfère toujours cloner le communicateur global
+	// MPI_COMM_WORLD qui gère l'ensemble des processus lancés par MPI.
+	MPI_Comm globComm;
+	MPI_Comm_dup(MPI_COMM_WORLD, &globComm);
+	// On interroge le communicateur global pour connaître le nombre de processus
+	// qui ont été lancés par l'utilisateur :
+	int nbp;
+	MPI_Comm_size(globComm, &nbp);
+	// On interroge le communicateur global pour connaître l'identifiant qui
+	// m'a été attribué ( en tant que processus ). Cet identifiant est compris
+	// entre 0 et nbp-1 ( nbp étant le nombre de processus qui ont été lancés par
+	// l'utilisateur )
+	int rank;
+	MPI_Comm_rank(globComm, &rank);
+	int nb = 700000;
+	double pi = approximate_pi(nb);
+    	std::stringstream fileName;
+	fileName << "Output" << std::setfill('0') << std::setw(5) << rank << ".txt";
+	std::ofstream output( fileName.str().c_str() );
+    	if (rank == 0) 
+    	{
+    		//output<< "hani fl IF"<<std::endl;
+        	MPI_Status status;
+        	double buffer;
+        	int i = 1;
+        	std::cout << "reception de la source : " << 0 << " valeur " << pi << std::endl;
+        	while (i < nbp) 
+        	{
+            		//output<<"hani fl while "<<std::endl;
+            		MPI_Recv(&buffer, 1, MPI_DOUBLE, MPI_ANY_SOURCE, 0, MPI_COMM_WORLD, &status);
+            		pi += buffer;
+            		output << "reception de la source : " << status.MPI_SOURCE << " valeur " << buffer << std::endl;
+            		i ++;
+        	}
+        	output<<"valeur finale de pi = "<< pi / nbp << std::endl;
+    	}
+    	else 
+    	{
+        
+		MPI_Request request;
+		MPI_Isend(&pi, 1, MPI_DOUBLE, 0, 0, MPI_COMM_WORLD, &request);
+		output << "la source : " << rank << " revoie la valeur " << pi << std::endl;
+        }
+
+	
+	output.close();
+	// A la fin du programme, on doit synchroniser une dernière fois tous les processus
+	// afin qu'aucun processus ne se termine pendant que d'autres processus continue à
+	// tourner. Si on oublie cet instruction, on aura une plantage assuré des processus
+	// qui ne seront pas encore terminés.
+	MPI_Finalize();
+	return EXIT_SUCCESS;
+}
