b90d8efbb7b64705c498badb787bedeb9f56c87f
Mustapha Bedoui <93264832+Hustler-labX@users.noreply.github.com>
2021-11-25 02:26:57 +0100

    Add files via upload

---
+++ __TP2__matrice–vecteur.cpp
@@ -0,0 +1,129 @@
+// Produit matrice-vecteur
+# include <cassert>
+# include <vector>
+# include <iostream>
+# include <mpi.h>
+#include<math.h>
+// ---------------------------------------------------------------------
+class Matrix : public std::vector<double>
+{
+public:
+    Matrix (int dim);
+    Matrix( int nrows, int ncols );
+    Matrix( const Matrix& A ) = delete;
+    Matrix( Matrix&& A ) = default;
+    ~Matrix() = default;
+
+    Matrix& operator = ( const Matrix& A ) = delete;
+    Matrix& operator = ( Matrix&& A ) = default;
+    void remplissage (int nrows, int ncols, int rank );
+    double& operator () ( int i, int j ) {
+        return m_arr_coefs[i + j*m_nrows];
+    }
+    double  operator () ( int i, int j ) const {
+        return m_arr_coefs[i + j*m_nrows];
+    }
+    
+    std::vector<double> operator * ( const std::vector<double>& u ) const;
+    
+    std::ostream& print( std::ostream& out ) const
+    {
+        const Matrix& A = *this;
+        out << "[\n";
+        for ( int i = 0; i < m_nrows; ++i ) {
+            out << " [ ";
+            for ( int j = 0; j < m_ncols; ++j ) {
+                out << A(i,j) << " ";
+            }
+            out << " ]\n";
+        }
+        out << "]";
+        return out;
+    }
+private:
+    int m_nrows, m_ncols;
+    std::vector<double> m_arr_coefs;
+};
+// ---------------------------------------------------------------------
+inline std::ostream& operator << ( std::ostream& out, const Matrix& A )
+{
+    return A.print(out);
+}
+// ---------------------------------------------------------------------
+inline std::ostream& operator << ( std::ostream& out, const std::vector<double>& u )
+{
+    out << "[ ";
+    for ( const auto& x : u )
+        out << x << " ";
+    out << " ]";
+    return out;
+}
+// ---------------------------------------------------------------------
+std::vector<double> Matrix::operator * ( const std::vector<double>& u ) const
+{
+    const Matrix& A = *this;
+    assert( u.size() == unsigned(m_ncols) );
+    std::vector<double> v(m_nrows, 0.);
+    for ( int i = 0; i < m_nrows; ++i ) {
+        for ( int j = 0; j < m_ncols; ++j ) {
+            v[i] += A(i,j)*u[j];
+        }            
+    }
+    return v;
+}
+// =====================================================================
+Matrix::Matrix (int dim) : m_nrows(dim), m_ncols(dim),
+                           m_arr_coefs(dim*dim)
+{
+    for ( int i = 0; i < dim; ++ i ) {
+        for ( int j = 0; j < dim; ++j ) {
+            (*this)(i,j) = (i+j)%dim;
+        }
+    }
+}
+// ---------------------------------------------------------------------
+Matrix::Matrix( int nrows, int ncols ) : m_nrows(nrows), m_ncols(ncols),
+                                         m_arr_coefs(nrows*ncols)
+{
+    int dim = (nrows > ncols ? nrows : ncols );
+    for ( int i = 0; i < nrows; ++ i ) {
+        for ( int j = 0; j < ncols; ++j ) {
+            (*this)(i,j) = (i+j)%dim;
+        }
+    }    
+}
+
+// =====================================================================
+int main( int nargs, char* argv[] )
+{
+	MPI_Init( &nargs, &argv );
+
+	MPI_Comm globComm;
+	MPI_Comm_dup(MPI_COMM_WORLD, &globComm);
+	int nbp;
+	MPI_Comm_size(globComm, &nbp);
+	int rank;
+	MPI_Comm_rank(globComm, &rank);
+	MPI_Status status ;
+	    MPI_Op count;
+	rank = 1	;
+	nbp=2;
+    const int N = 12;
+    int partition(N/nbp);
+        std::vector<double> u( N );
+            std::vector<double> resultat( N );
+    std::vector<double> tab( N );
+    for (int i = 0 ; i < N ; i++){
+        v_part[i] = 0;
+        for (int j = partition*rank ; j < partition*(rank+1) ; j++){
+            tab[i] += A(i,j)*u[j];
+        }
+    }
+    MPI_Allreduce (&tab[0], &resultat[0], N, MPI_DOUBLE, count, MPI_COMM_WORLD); // je n'ai pas arriver à faire le count
+    
+
+    std::cout << "A.u = " << resultat << std::endl;
+    */
+    MPI_Finalize();
+    return EXIT_SUCCESS;
+}
